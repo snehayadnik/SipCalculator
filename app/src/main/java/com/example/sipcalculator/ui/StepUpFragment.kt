5262f4ceac450250429124f4e08454ba62b96fc4
YADNISS <snehayadnik9@gmail.com>
2024-06-17 16:18:53 +0530

    Updated UI

--- app/src/main/java/com/example/sipcalculator/ui/StepUpFragment.kt
+++ app/src/main/java/com/example/sipcalculator/ui/StepUpFragment.kt
@@ -24,8 +24,8 @@ class StepUpFragment : Fragment() {
     private lateinit var inputTime: EditText
     private lateinit var calculateBtn: Button
     private lateinit var calculatedReturnsText: TextView
-    private var calculatedAmt:Int=0
-    private var invAmt:Int=0
+    private var calculatedAmt: Int = 0
+    private var invAmt: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,14 +47,15 @@ class StepUpFragment : Fragment() {
             calculateBtn = calculateButtonStepUp
             calculatedReturnsText = totalInvStepUp
         }
-setData()
+        setData()
 
         calculateBtn.setOnClickListener {
             val enteredAmt: Int = Integer.parseInt(inputAmount.text.toString())
             val enteredRate: Int = Integer.parseInt(inputReturnRate.text.toString())
             val enteredTime: Int = Integer.parseInt(inputTime.text.toString())
-            calculatedAmt = ((enteredAmt * ((((1 + enteredRate) * enteredTime) - 1) / enteredRate) * (1 + enteredRate)).toDouble()).toInt()
-           invAmt=enteredAmt*enteredTime
+            calculatedAmt =
+                ((enteredAmt * ((((1 + enteredRate) * enteredTime) - 1) / enteredRate) * (1 + enteredRate)).toDouble()).toInt()
+            invAmt = enteredAmt * enteredTime
             calculatedReturnsText.text = calculatedAmt.toString()
             setData()
         }
@@ -62,27 +63,28 @@ setData()
     }
 
 
+    private fun setData() {
+        investment.text = invAmt.toString()
+        estimated.text = calculatedAmt.toString()
 
-private fun setData() {
-    investment.text = invAmt.toString()
-    estimated.text = calculatedAmt.toString()
 
+        // Set the data and color to the pie chart
+        pieChart.addPieSlice(
+            PieModel(
+                "Invested Amount",
+                Integer.parseInt(investment.text.toString()).toFloat(),
+                Color.parseColor("#FCD828")
+            )
+        )
+        pieChart.addPieSlice(
+            PieModel(
+                "Estimated Amount",
+                Integer.parseInt(estimated.text.toString()).toFloat(),
+                Color.parseColor("#1A7CC8")
+            )
+        )
 
-    // Set the data and color to the pie chart
-    pieChart.addPieSlice(
-        PieModel(
-            "Invested Amount",
-            Integer.parseInt(investment.text.toString()).toFloat(),
-            Color.parseColor("#FCD828"))
-    )
-    pieChart.addPieSlice(
-        PieModel(
-            "Estimated Amount",
-            Integer.parseInt(estimated.text.toString()).toFloat(),
-            Color.parseColor("#1A7CC8"))
-    )
-
-    // To animate the pie chart
-    pieChart.startAnimation()
-}
+        // To animate the pie chart
+        pieChart.startAnimation()
     }
+}
